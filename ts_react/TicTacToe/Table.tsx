7b415bc288481a612e5f2e104b7a1a22786bd0ce
momo <momo@momoui-Macmini.local>
2021-03-18 18:31:52 +0900

    feat: Lotto~MineSearch - useContext - useReducer

---
+++ ts_react/TicTacToe/Table.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { Dispatch, FunctionComponent, useMemo } from "react";
+import Tr from "./Tr";
+interface Props {
+  tableData: string[][];
+  dispatch: Dispatch<any>;
+  onClick: () => void;
+}
+
+const Table: FunctionComponent<Props> = ({ tableData, dispatch }) => {
+  return (
+    <table>
+      <tbody>
+        {Array(tableData.length)
+          .fill(null)
+          .map((tr, i) =>
+            useMemo(
+              () => (
+                <Tr
+                  key={i}
+                  dispatch={dispatch}
+                  rowIndex={i}
+                  rowData={tableData[i]}
+                />
+              ),
+              [tableData[i]]
+            )
+          )}
+      </tbody>
+    </table>
+  );
+};
+export default Table;
---
+++ ts_react/TicTacToe/Tr.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { memo, useMemo, FC, Dispatch } from "react";
+import Td from "./Td";
+interface Props {
+  rowData: string[];
+  rowIndex: number;
+  dispatch: Dispatch<any>;
+}
+const Tr: FC<Props> = ({ rowData, rowIndex, dispatch }) => {
+  console.log("tr rendered");
+  return (
+    <tr>
+      {Array(rowData.length)
+        .fill(null)
+        .map((td, i) =>
+          useMemo(
+            () => (
+              <Td
+                key={i}
+                dispatch={dispatch}
+                rowIndex={rowIndex}
+                cellIndex={i}
+                cellData={rowData[i]}
+              >
+                {""}
+              </Td>
+            ),
+            [rowData[i]]
+          )
+        )}
+    </tr>
+  );
+};
+export default memo(Tr);
